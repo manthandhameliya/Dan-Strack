1428b493ea183846206d1db5163ddb183e1529c1
jenis <jenis@hectorai.live>
2024-12-26 14:42:28 +0530

    added changes

--- src/Components/Product.jsx
+++ src/Components/Product.jsx
@@ -26,6 +26,7 @@ const Product = () => {
   const [isSidebarOpen, setIsSidebarOpen] = useState(true);
   const [activeTab, setActiveTab] = useState("dashboard");
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+  useEffect(() => {})
   const [language, setLanguage] = useState("English");
   const handleLanguageChange = (newLanguage) => {
     setLanguage(newLanguage);
